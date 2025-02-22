18f7016ba2d176854df1798a662b62f9b7717038
zeeshansiddiqui <zeeshansiddiqui.bscs@gmail.com>
2025-02-22 19:13:47 +0500

    in second script add 1 debug name zeeshan

--- Assets/Scenes/NewBehaviourScript1.cs
+++ Assets/Scenes/NewBehaviourScript1.cs
@@ -7,7 +7,7 @@ public class NewBehaviourScript1 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("zeeshan");
     }
 
     // Update is called once per frame
