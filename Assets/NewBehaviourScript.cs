152df49f5a1278e2cdd6609cab36212964594fef
zeeshansiddiqui <zeeshansiddiqui.bscs@gmail.com>
2025-02-22 20:10:20 +0500

    Change Version
    i jusr create first blank script and then change version unity

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -7,7 +7,7 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("zeeshan ny chaning ki ha");   
     }
 
     // Update is called once per frame
